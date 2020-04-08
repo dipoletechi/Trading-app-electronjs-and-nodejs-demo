a13376df32648ec1a683909ef8c50ee2ae531bf0
Dharmandar Kumar <dipoletechi@gmail.com>
2020-04-08 10:54:38 +0530

    fixed build issue

--- UKTradingDesktopApp/src/app/shared/helpers/apiurl.helper.ts
+++ UKTradingDesktopApp/src/app/shared/helpers/apiurl.helper.ts
@@ -2,7 +2,7 @@
 export class APIUrlHelper {
 
   //Production api link 
-  public static ApiEndPoint: string = "http://159.65.19.158:3000";
+  public static ApiEndPoint: string = "http://112.196.0.251:3000";
   //public static ApiEndPoint:string="http://localhost:3000";
   public static WatchlistApiEndPoint: string = APIUrlHelper.ApiEndPoint + "/api/watchlist";
   public static AutenticationApiEndPoint: string = APIUrlHelper.ApiEndPoint + "/api/auth";
